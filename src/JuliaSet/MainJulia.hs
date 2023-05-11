8b96b9c3d0406935b5cbd04c00544df6fd894935
SiryZex <219120756@student.uj.ac.za>
2023-05-12 01:33:52 +0200

    ADDED: Added guards to Julia Set
    Added guard conditions to Julia Set to make it allow users to choose what set they would like to view

--- src/JuliaSet/MainJulia.hs
+++ src/JuliaSet/MainJulia.hs
@@ -1,15 +1,30 @@
 import JuliaSet
 import Data.Complex
+
 main = do
-    -- let c = (0.45) :+ (-0.1428)
-    -- let c = (0.285 :+ 0.01)
-    -- let c = (-0.45) :+ (-0.143) -- hazycloud
-    -- let c = (-0.54) :+ (0.54) -- dragon
-    -- let c = 0 :+ (0.8) -- veins
-    let c = 0.355 :+ (0.355) -- swirlybois
-    -- let c = (-0.8) :+ 0.156
-        pixels = [[julia c (pixelToComplex (x, y)) | x <- [0..width-1]] | y <- [0..height-1]]
-        colors = map (map color) pixels
-        ppm = "P3\n" ++ show width ++ " " ++ show height ++ "\n255\n" ++
-              unlines (map (unwords . map (\(r,g,b) -> show r ++ " " ++ show g ++ " " ++ show b)) colors)
+    putStrLn $ "Choose the number for Julia Fractal you would like to view:\n" ++
+        "1) bacteria\n" ++
+        "2) 4-leaf clover\n" ++
+        "3) hazycloud\n" ++
+        "4) dragon\n" ++
+        "5) veins\n" ++
+        "6) swirlybois\n"++
+        "7) cold breath touch\n"
+    setFractal <- getLine
+    let c = fractal (read setFractal)
+    let pixels = [[julia c (pixelToComplex (x, y)) | x <- [0..width-1]] | y <- [0..height-1]]
+    let colors = map (map color) pixels
+    let ppm = "P3\n" ++ show width ++ " " ++ show height ++ "\n255\n" ++
+            unlines (map (unwords . map (\(r,g,b) -> show r ++ " " ++ show g ++ " " ++ show b)) colors)
     writeFile "julia.ppm" ppm
+
+fractal :: Int -> Complex Double
+fractal c
+    | c == 1 = (0.45) :+ (-0.1428) -- bacteria
+    | c == 2 = (0.285 :+ 0.01) -- 4-leaf clover
+    | c == 3 = (-0.45) :+ (-0.143) -- hazycloud
+    | c == 4 = (-0.54) :+ (0.54) -- dragon
+    | c == 5 = 0 :+ (0.8) -- veins
+    | c == 6 = 0.355 :+ (0.355) -- swirlybois
+    | c == 7 = (-0.8) :+ 0.156 -- cold breath touch
+    | otherwise = error "Invalid choice"
